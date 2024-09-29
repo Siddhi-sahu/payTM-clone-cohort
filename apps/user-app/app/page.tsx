fdaa133f86d896c21edba0e0322a882f3d9975b3
Siddhi Sahu <himanisahu739@gmail.com>
2024-09-29 16:43:50 +0530

    recoil imports corrected

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 
+import { useBalance } from "@repo/store/useBalance"
 export default function Home() {
+  const balance = useBalance();
   return (
-    <div className="bg-slate-600">hii there</div>
+    <div className="bg-slate-600">hii there {balance}</div>
   );
 }
