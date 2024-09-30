76cb27c9091dcb95b40b7e2bde9c071aece9c547
Siddhi Sahu <himanisahu739@gmail.com>
2024-09-30 14:57:11 +0530

    merchant data in db success

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -1,11 +1,15 @@
 "use client"
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
 
     return <RecoilRoot>
-        {children}
+        <SessionProvider>
+
+            {children}
+        </SessionProvider>
     </RecoilRoot>
 
 }
